271de3dca624f05c0072eef6f95d0ef05a1e2d51
Uraganchik1983 <capability@capability.org.ua>
2018-12-23 22:10:24 +0200

    testing web services

---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WcfService1;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var clients = new WcfService1.WebService1();
+
+            Label1.Text = clients.GetSum(int.Parse(TextBox1.Text), int.Parse(TextBox2.Text)).ToString();
+        }
+    }
+}
